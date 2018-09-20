6ad55ae1bd229decf7e17ae425845f0d63c298e5
christdbrown77 <christdbrown77@gmail.com>
2018-09-20 20:15:05 +0100

    Commit - with routing

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -6,15 +6,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-
+import { AboutComponent } from './about/about.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpModule
+    BrowserModule, FormsModule, HttpModule, routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- myApp/src/app/user/user.component.ts
+++ myApp/src/app/user/user.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  title = 'my app';
   name = 'Chris Brown';
   email = 'christdbrown77@gmail.com';
   hobbies = ['Soccer', 'Athletics', 'Golf', 'GAA'];
