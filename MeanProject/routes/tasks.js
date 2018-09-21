91924a153f7342e964d634ec6d1c110a3783037f
christdbrown77 <christdbrown77@gmail.com>
2018-09-21 21:26:55 +0100

    Database created and connected

--- MeanProject/routes/tasks.js
+++ MeanProject/routes/tasks.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://christdbrown77:christdbrown77@ds111623.mlab.com:11623/tasklist',['tasks']);
 
+// Get All Tasks
 router.get('/tasks', function(req, res, next){
-    res.send('Tasks Api');
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    })
 });
+
+// Get Single Task
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    })
+});
+
 
 module.exports = router;
