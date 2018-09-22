595c6434d9b70d8574c77ea88738ba2314b8dfd6
christdbrown77 <christdbrown77@gmail.com>
2018-09-22 16:59:08 +0100

    Final

--- MeanProject/routes/tasks.js
+++ MeanProject/routes/tasks.js
@@ -25,5 +25,67 @@ router.get('/task/:id', function (req, res, next) {
     })
 });
 
+// Posts a task
+router.post('/task', function (req, res, next) {
+    var task = req.body;
+    if (!task.title || (task.isDone + '')) {
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.tasks.save(task, function (err, task) {
+            if (err) {
+                res.sendFile(err);
+            }
+            res.json(task)
+        })
+    }
+})
+
+// Delete a task
+router.delete('/task/:id', function (req, res, next) {
+    db.tasks.remove({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function (err, task) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(task);
+    })
+});
+
+// Update a task
+router.put('/task/:id', function (req, res, next) {
+
+    var task = req.body;
+    var updateTask = {};
+
+    if (task.isDone) {
+        updateTask.isDone = task.isDone;
+    }
+
+    if (task.title) {
+        updateTask.title = task.title;
+    }
+
+    if (!updateTask) {
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.tasks.update({
+            _id: mongojs.ObjectId(req.params.id)
+        }, updateTask, {}, function (err, task) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }
+
+
+});
 
 module.exports = router;
