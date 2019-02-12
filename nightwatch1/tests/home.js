98f8b7099f1dae842078b09c220ae91280524445
christdbrown77 <christdbrown77@gmail.com>
2019-02-12 20:37:30 +0000

    update

--- nightwatch1/tests/home.js
+++
@@ -1,9 +0,0 @@
-module.exports = { 
-    'Prudential homepage': function(browser) {
-      browser
-        .url('https://www.prudential.com')
-        .waitForElementVisible('body')
-        .assert.title('Life Insurance, Retirement, Investments | Prudential Financial')
-        .end();
-    }
-  };
