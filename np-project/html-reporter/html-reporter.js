dcf942b646d4ef0cad2f64a1728df8b31fab3d3d
christdbrown77 <christdbrown77@gmail.com>
2019-04-14 16:07:15 +0100

    Update report format

--- np-project/html-reporter/html-reporter.js
+++ np-project/html-reporter/html-reporter.js
@@ -1,19 +1,17 @@
 var HtmlReporter = require('nightwatch-html-reporter');
-/* Same options as when using the built in nightwatch reporter option */
+
 var reporter = new HtmlReporter({
     /* True or False.  If true the generated html will be opened in your browser after the test run. */
     openBrowser: true,
 
-    /* The directory you've set nightwatch to store your reports.
-        On the CLI this determines where we read reports from, but on this
-        interface it determines where the generated report will be saved. */
+    /* The directory you've set nightwatch to store your reports.*/
     reportsDirectory: './reports',
 
     /* The filename that the html report will be saved as. */
     reportFilename: 'generatedReport.html',
 
     /* Boolean.  If true we ensure the generated report filename is unique by appending a timestamp to the end. */
-    uniqueFilename: false,
+    uniqueFilename: true,
 
     /* Boolean.  If true we append the last suite name to the report filename. */
     separateReportPerSuite: false,
@@ -26,9 +24,6 @@ var reporter = new HtmlReporter({
 
     /* If true then only errors will be shown in the report. */
     hideSuccess: false,
-
-    /* If true we append a timestamp to the end of the generated report filename */
-    uniqueFilename: false,
 
     /* If true we convert screenshot paths from absolute paths to relative to output file. */
     relativeScreenshots: false
